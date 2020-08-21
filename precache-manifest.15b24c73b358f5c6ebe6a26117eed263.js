678c8fbc6ce00b39556299f5f2f2be5d4f067cb0
Jobe Michael <jobumichael@gmail.com>
2020-08-21 11:40:00 +0200

    Updates

---
+++ precache-manifest.15b24c73b358f5c6ebe6a26117eed263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07adcbcb76408a65a6252d02a552c9cd",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "437af5f838033831be8d",
+    "url": "/weather-app/static/css/main.30ed8c60.chunk.css"
+  },
+  {
+    "revision": "215ba51009b54a6c3a64",
+    "url": "/weather-app/static/js/2.6143a530.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.6143a530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437af5f838033831be8d",
+    "url": "/weather-app/static/js/main.bdc45e80.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "f1ddd84e18c0bb2e087ad838d1791c99",
+    "url": "/weather-app/static/media/bg.f1ddd84e.png"
+  }
+]);
